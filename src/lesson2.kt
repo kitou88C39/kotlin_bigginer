25bda0ccff3343a6a002260921856bdbd7290828
kitoumasaomi <kanfutrooper@gmail.com>
2025-11-12 20:45:24 +0900

    lesson2.ktを作成する

--- src/lesson2.kt
+++ src/lesson2.kt
@@ -1,14 +1,17 @@
 fun main(){
+
     //変数
     var num = 10
-    println("num = $num")
-    
+    println("num = $num")    
     println(num::class)
 
     var str = "Hello"
     println("str = $str")
+    println(str::class)
 
+    num = 20
+    println(num)
 
-
-    //定数
+    val x = 20
+    x = 20
 }
