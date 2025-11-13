e5dd3aab93d376f0e298318f43dbb8e525dd61f1
kitoumasaomi <kanfutrooper@gmail.com>
2025-11-13 15:31:05 +0900

    lesson5.ktを作成する

---
+++ src/lesson5.kt
@@ -0,0 +1,20 @@
+fun main(){
+
+    val str: String? = null
+
+    // !!
+    val list: MutableList<String?> = mutableListOf(10, 20, 30)
+    val num: Int? = list.min()
+
+    // スマートキャスト
+    if(num != null){
+        println(num * 2)
+    }
+    // !!
+    val num2: Int = num!!
+    println(num2 * 2)
+
+    
+
+
+}
