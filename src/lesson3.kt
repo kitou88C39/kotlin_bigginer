a44be9a0f9ffbbbdaf4529d85fcb42f24686445c
kitoumasaomi <kanfutrooper@gmail.com>
2025-11-13 11:22:24 +0900

    lesson3.ktを作成する

--- src/lesson3.kt
+++ src/lesson3.kt
@@ -6,4 +6,11 @@ fun main(){
     println("x = ${x::class}")
     println("y = ${y::class}")
     println("z = ${z::class}")
+    //整数
+    val num1:Int = 10 //32bit
+    val num2:Long = 10 //64bit
+    val num3:Short = 10 //16bit
+    val num4:Byte = 10 //8bit
+    //小数
+
 }
