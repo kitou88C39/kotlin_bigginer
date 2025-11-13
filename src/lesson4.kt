357972755165489e83c1c096fe319d42300af5f8
kitoumasaomi <kanfutrooper@gmail.com>
2025-11-13 15:05:49 +0900

    lesson4.ktを作成する

---
+++ src/lesson4.kt
@@ -0,0 +1,17 @@
+fun main(){
+
+    val x: Int = 10
+    val y: Long = x.toLong()
+    
+    val d: Double = x.toDouble()
+    val f: Float = d.toFloat()
+
+    println(y)
+    println(d)
+    println(f)
+
+    val dd = 1.6
+    val i:int = dd.toInt()
+    println(i)
+
+}
