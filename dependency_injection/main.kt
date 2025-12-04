f5fe6d125847b90ac1ca3a96bb23388fd65ce302
kitoumasaomi <kanfutrooper@gmail.com>
2025-12-04 15:59:50 +0900

    main.ktを作成

---
+++ dependency_injection/main.kt
@@ -0,0 +1,17 @@
+class ClassA {
+    val propA = "The property of ClassA"
+}
+
+class classB {
+    val instanceA = ClassA()
+    fun methodB() {
+        println()
+    }
+}
+
+fun main(){
+    val kotlin = "smile"
+    println(classA.propA)
+}
+
+    
